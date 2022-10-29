d6bc5e130b5c42072f44753ae36b35ca4c079b01
David Robb <66834033+drobb2020@users.noreply.github.com>
2022-10-29 11:05:53 -0400

    Added css and scss loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 let mode = 'development'
+let target = 'web'
 
 if (process.env.NODE_ENV === 'production') {
   mode = 'production'
+  target = 'browserslist'
 }
 
 module.exports = {
   mode: mode,
+  target: target,
 
   module: {
     rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -20,6 +33,8 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [new MiniCssExtractPlugin()],
 
   devtool: 'source-map',
   devServer: {
